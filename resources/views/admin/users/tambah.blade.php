5bca37ce71ba2a1ac214ea284e1e40bb6fc0b1aa
jomphp <gwbiz4u@gmail.com>
2016-08-11 09:48:34 +0800

    Update Form Tambah User

--- resources/views/admin/users/tambah.blade.php
+++ resources/views/admin/users/tambah.blade.php
@@ -1,1 +1,39 @@
+@extends('admin/layout')
+
+@section('content')
+
 <h1>Tambah User</h1>
+
+<hr>
+
+{!! Form::open() !!}
+
+  <div class="form-group">
+    <label>Username</label>
+    {!! Form::text('username', null, ['class' => 'form-control']) !!}
+  </div>
+  <div class="form-group">
+    <label>Email</label>
+    {!! Form::email('username', null, ['class' => 'form-control']) !!}
+  </div>
+  <div class="form-group">
+    <label>Name</label>
+    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+  </div>
+  <div class="form-group">
+    <label>Password</label>
+    {!! Form::password('password', ['class' => 'form-control']) !!}
+  </div>
+  <div class="form-group">
+    <label>Password Confirmation</label>
+    {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+  </div>
+  <div class="form-group">
+    <label>Role</label>
+    {!! Form::select('username', array('admin' => 'Admin', 'user' => 'User'), null, ['class' => 'form-control']) !!}
+  </div>
+  <button type="submit" class="btn btn-primary">Daftar Masuk</button>
+
+{!! Form::close() !!}
+
+@endsection
